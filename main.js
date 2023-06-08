a8e5dcf14b66111d33cd39d9d11cb1a46390c923
Rakodashi <rafaconrado1@gmail.com>
2023-06-07 23:22:23 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var listaFilmes = [
+	"https://m.media-amazon.com/images/M/MV5BYzMzZmE3MTItODYzYy00YWI5LWFkNWMtZTY5NmU2MDkxYWI1XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_FMjpg_UX1000_.jpg",
+	"https://blyme-yaoi.com/2018/wp-content/uploads/2019/10/EHsI5aCUUAAuTif.jpg",
+	"https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2017/05/koe-no-katachi-a-voz-do-silencio1-1.webp?ssl=1",
+	"https://www.comingsoon.net/assets/uploads/2021/11/heaven-officials-blessing-season-2.jpg",
+	"https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+	"https://m.media-amazon.com/images/M/MV5BMjQ0NTI0NjkyN15BMl5BanBnXkFtZTgwNzY0MTE0NzM@._V1_.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+	document.write("<img src=" + listaFilmes[i] + ">");
+}
